2f572ea76a38ef66b7bad2ae08e4590ce0eb0fc5
Jim Rodovich <jim@inventables.com>
2014-09-26 15:20:55 -0500

    Implement lawn policy.

--- grassist.js
+++ grassist.js
@@ -1,41 +1,15 @@
-var lawnmower = require('./lib/lawnmower.js');
-
-console.log(lawnmower.heading());
-console.log(lawnmower.position());
-console.log(lawnmower.rotorEnabled());
-
-lawnmower.turnLeft();
-console.log(lawnmower.heading());
-lawnmower.turnLeft();
-console.log(lawnmower.heading());
-lawnmower.turnLeft();
-console.log(lawnmower.heading());
-lawnmower.turnLeft();
-console.log(lawnmower.heading());
-
-lawnmower.turnRight();
-console.log(lawnmower.heading());
-lawnmower.turnRight();
-console.log(lawnmower.heading());
-lawnmower.turnRight();
-console.log(lawnmower.heading());
-lawnmower.turnRight();
-console.log(lawnmower.heading());
-
-lawnmower.moveForward();
-console.log(lawnmower.position());
+var lawn = require('./lib/lawn.js')(["XXXXX", "X !!X", "X %!X", "XXXXX"], 1, 2);
+var lawnmower = require('./lib/lawnmower.js')(lawn);
 
 lawnmower.startRotor();
-console.log(lawnmower.rotorEnabled());
+lawnmower.moveForward();
+lawnmower.moveForward();
+lawnmower.turnRight();
+lawnmower.moveForward();
 lawnmower.stopRotor();
-console.log(lawnmower.rotorEnabled());
-
-var lawn = require('./lib/lawn.js')(["XXXXX", "X!!!X", "X .%X", "XXXXX"], 3, 1);
-
-console.log(lawn.patchType(-2, 1));
-console.log(lawn.patchType(-2, 0));
-console.log(lawn.patchType(-1, 0));
-console.log(lawn.patchType(0, 0));
-
-console.log(lawn.bounds());
+lawnmower.turnRight();
+lawnmower.moveForward();
+lawnmower.moveForward();
+lawnmower.turnRight();
+lawnmower.moveForward();
 
