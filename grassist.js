f40469bbeaa24cfd7975e1a7ff02fc3bd0822eee
Jim Rodovich <jim@inventables.com>
2014-09-26 15:49:09 -0500

    Rename `lawn` to `yard` to match the docs.
    (According to the dictionary, a lawn is just the grassy part of a yard.)

--- grassist.js
+++ grassist.js
@@ -1,13 +1,13 @@
-var lawn = require('./lib/lawn.js')(["XXXXX", "X !!X", "X %!X", "XXXXX"], 1, 2);
-var lawnmower = require('./lib/lawnmower.js')(lawn);
+var yard = require('./lib/yard.js')(["XXXXX", "X !!X", "X %!X", "XXXXX"], 1, 2);
+var lawnmower = require('./lib/lawnmower.js')(yard);
 
 lawnmower.startRotor();
 lawnmower.moveForward();
 lawnmower.moveForward();
 lawnmower.turnRight();
-console.log(lawn.freshlyCut());
+console.log(yard.freshlyCut());
 lawnmower.moveForward();
-console.log(lawn.freshlyCut());
+console.log(yard.freshlyCut());
 lawnmower.stopRotor();
 lawnmower.turnRight();
 lawnmower.moveForward();
