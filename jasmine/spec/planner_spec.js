a5f684c1be6b6c7618dfdadea55c85b2c39c66d0
Jim Rodovich <jim@inventables.com>
2014-09-29 16:08:54 -0500

    Add more tests.

--- jasmine/spec/planner_spec.js
+++ jasmine/spec/planner_spec.js
@@ -1,16 +1,104 @@
 describe("planner", function() {
   var planner;
 
+  var mowMatcher = {
+    toMow: function() {
+      return {
+        compare: function(planner, patches, homeLongitude, homeLatitude) {
+          var yard = GRASSIST.yard(patches, homeLongitude, homeLatitude);
+          var mower = GRASSIST.lawnmower(yard);
+          var messages = [];
+          planner.plan(mower, yard);
+          if (!yard.freshlyCut()) {
+            messages.push("Expected planner to cut the whole yard");
+          }
+          var position = mower.position();
+          if (position[0] !== 0 || position[1] !== 0) {
+            messages.push("Expected the lawnmower to be returned to its home position");
+          };
+          if (mower.rotorEnabled()) {
+            messages.push("Expected the lawnmower's rotor to be powered off");
+          }
+          return {
+            pass: messages.length === 0,
+            message: messages.join(", ")
+          };
+        }
+      };
+    }
+  };
+
   beforeEach(function() {
+    jasmine.addMatchers(mowMatcher);
     planner = GRASSIST.planner();
   });
 
+  // ! long grass
+  // . short grass
+  //   sidewalk
+  // % mulch or gravel
+  // X plant or flower
+
   it("should mow a tiny yard", function() {
-    var yard = GRASSIST.yard(["!"], 0, 0);
-    var mower = GRASSIST.lawnmower(yard);
-    planner.plan(mower, yard);
-    expect(yard.freshlyCut()).toBe(true);
-    expect(mower.rotorEnabled()).toBe(false);
-    expect(mower.position()).toEqual([0, 0]);
+    var patches = [
+      "!"
+    ];
+    expect(planner).toMow(patches, 0, 0);
+  });
+
+  it("should mow a small yard", function() {
+    var patches = [
+      "!!!",
+      "!!!"
+    ];
+    expect(planner).toMow(patches, 0, 0);
+  });
+
+  it("should mow a large yard", function() {
+    var patches = [
+      "!!!!!!!!",
+      "!!!!!!!!",
+      "!!!!!!!!",
+      "!!!!!!!!"
+    ];
+    expect(planner).toMow(patches, 0, 0);
+  });
+
+  it("should avoid gravel when the rotor is enabled", function() {
+    var patches = [
+      "!!!!%!!!!",
+      "!!!!%!!!!",
+      "%%%%%%%%%",
+      "!!!!%!!!!",
+      "!!!!%!!!!"
+    ];
+    expect(planner).toMow(patches, 0, 4);
+  });
+
+  it("should avoid passing over a flower bed", function() {
+    var patches = [
+      "!XXXXXX!",
+      "!!XXXX!!",
+      "!!!!!!!!",
+      "!!!!!!!!",
+      "!!!!!!!!"
+    ];
+    expect(planner).toMow(patches, 4, 0);
+  });
+
+  it("should mow a complicated yard", function() {
+    var patches = [
+      "XXXXXXXXXXXXX",
+      "XXXX!!!!!!!!X",
+      "XXX!!!!X!!!!X",
+      "XX!!!!!!!!!!X",
+      " !!!!!!!!!!XX",
+      " !!!!!!!%%%%%",
+      " !!!!!!%%%%%%",
+      "  !!!!%%%%%%%",
+      "             "
+    ];
+    expect(planner).toMow(patches, 0, 0);
   });
 });
+
